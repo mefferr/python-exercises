ec99028205e9864564342ee2a3f4238a99ecf478
mefer <53373208+mefferr@users.noreply.github.com>
2023-04-02 14:00:06 +0200

    pierwsza część zadania

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+names1 = ["Mateusz", "Fabian", "Laura", "Adrian"]
+names2 = ["Katarzyna", "Adam", "Ewelina", "Tomasz"]
+names3 = ["Aleksandra", "Ewa", "Andrzej", "Michał"]
+
+set1 = set(names1)
+set2 = set(names2)
+set3 = set(names3)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Set 3:", set3)
